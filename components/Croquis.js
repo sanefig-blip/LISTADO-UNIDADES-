d6b87a542c42611af13af837c53fa580de5f344a
sanefig-blip <sanefig@gmail.com>
2025-09-20 11:22:54 -0300

    feat: Update map tile layer to dark theme
    Replaces the default OpenStreetMap tile layer with a dark theme variant from CARTO. This improves map readability in low-light conditions and provides a more visually consistent interface. Also removes unused dark theme CSS specific to Leaflet tiles.

--- components/Croquis.js
+++ components/Croquis.js
@@ -243,8 +243,8 @@ const Croquis = forwardRef((props, ref) => {
             mapRef.current = map;
             drawnItemsRef.current = new L.FeatureGroup().addTo(map);
 
-            const streetLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            const streetLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             });
             const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', { attribution: '&copy; Esri' });
             const baseLayers = { 'Calles': streetLayer, 'Satélite': satelliteLayer };
